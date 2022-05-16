40cf953a0ca2502d01c6d5bc5314ab929a4270c7
Tin Kei Cheng <tinkei.cheng@tum.de>
2022-05-16 02:55:48 +0200

    Execute motion planner every n time steps.

--- simulation/simulations.py
+++ simulation/simulations.py
@@ -12,9 +12,11 @@ __status__ = "Integration"
 
 import copy
 import os
+import sys
 import pickle
 from enum import unique, Enum
 from math import sin, cos
+from pprint import pp
 from typing import Tuple, Dict, Optional
 
 import numpy as np
@@ -29,6 +31,32 @@ from sumocr.sumo_config.default import DefaultConfig
 from sumocr.sumo_docker.interface.docker_interface import SumoInterface
 from sumocr.visualization.video import create_video
 
+
+path_notebook = os.getcwd()
+sys.path.append(os.path.join(path_notebook, 'commonroad-search/'))
+sys.path.append(os.path.join(path_notebook, 'commonroad-search/SMP/'))
+
+from maneuver_automaton.maneuver_automaton import ManeuverAutomaton
+from SMP.motion_planner.utility import plot_primitives
+from MotionPrimitiveGenerator import MotionPrimitiveGenerator as MPG
+from commonroad.scenario.trajectory import State, Trajectory
+from commonroad.common.solution import CostFunction
+from SMP.motion_planner.motion_planner import MotionPlanner, MotionPlannerType
+from SMP.motion_planner.utility import create_trajectory_from_list_states, visualize_solution
+
+@unique
+class Algo(Enum):
+    """Enum for search algo used."""
+    ASTAR = 1
+    GBFS = 2
+algo = Algo.ASTAR
+# algo = Algo.GBFS
+
+# Primitives 389 (w/ mirror), or 270. Branching factor 5.565.
+automaton = ManeuverAutomaton.generate_automaton('my_motion_primitives/V_0.0_42.0_Vstep_3.0_SA_-0.91_0.91_SAstep_0.15_T_0.5_Model_FORD_ESCORT.xml')
+
+# Primitives 53 (w/ mirror), or 53. Branching factor 1.0.
+# automaton = ManeuverAutomaton.generate_automaton('my_motion_primitives/V_0.0_42.0_Vstep_3.0_SA_-0.91_0.91_SAstep_0.15_T_0.1_Model_FORD_ESCORT.xml')
 
 @unique
 class SimulationOption(Enum):
@@ -85,31 +113,121 @@ def simulate_scenario(mode: SimulationOption,
     elif mode is SimulationOption.MOTION_PLANNER:
         # simulation with plugged in planner
 
+        trajectory_buffer = []
+        pp_orig_init_states = {}
+        pp_orig_time_starts = {}
+        pp_orig_time_ends = {}
+
+        def get_next_state(current_scenario, trajectory_buffer, pp_id, step, skip_step=33):
+            """Run motion planner only ever n steps. Otherwise return previous trajectory."""
+
+            if step % skip_step == 0:
+
+                trajectory_buffer.clear()
+
+                # Backup initial state before modification.
+                if pp_id not in pp_orig_init_states:
+                    pp_orig_init_states[pp_id] = planning_problem_set.planning_problem_dict[pp_id].initial_state
+                    pp_orig_time_starts[pp_id] = planning_problem_set.planning_problem_dict[pp_id].goal.state_list[0].time_step.start
+                    pp_orig_time_ends[pp_id]   = planning_problem_set.planning_problem_dict[pp_id].goal.state_list[0].time_step.end
+                    planning_problem_set.planning_problem_dict[pp_id].goal.state_list[0].time_step.start += 1
+                    planning_problem_set.planning_problem_dict[pp_id].goal.state_list[0].time_step.end   += 1
+
+                # Construct motion planner.
+                if algo == Algo.ASTAR:
+                    type_motion_planner = MotionPlannerType.STUDENT
+                elif algo == Algo.GBFS:
+                    type_motion_planner = MotionPlannerType.STUDENT_GBFS
+
+                motion_planner = MotionPlanner.create(scenario=current_scenario, 
+                                                    # Each ego vehicle corresponds to a planning problem! See `init_ego_vehicles_from_planning_problem()`.
+                                                    planning_problem=planning_problem_set.planning_problem_dict[pp_id],
+                                                    automaton=automaton, 
+                                                    motion_planner_type=type_motion_planner)
+
+                list_paths_primitives, _, _ = motion_planner.execute_search()
+                trajectory_ego = create_trajectory_from_list_states(list_paths_primitives)
+                print(f'Length of trajectory states: {len(trajectory_ego.state_list)}')
+                print(f'Solution trajectory time step: {trajectory_ego.state_list[-1].time_step}')
+
+                if len(trajectory_ego.state_list) > 1:
+
+                    trajectory_buffer.extend(trajectory_ego.state_list[1:])
+                    # next_state = trajectory_ego.state_list[1]
+                    next_state = trajectory_buffer[0]
+
+                    next_initial_state = copy.deepcopy(trajectory_buffer[:skip_step][-1])
+                    next_initial_state.time_step = 0
+                    # Add back deleted unnecessary attributes of the initial state
+                    if not hasattr(next_initial_state, 'yaw_rate'):
+                        next_initial_state.yaw_rate = 0.0
+                    if not hasattr(next_initial_state, 'slip_angle'):
+                        next_initial_state.slip_angle = 0.0
+                    if not hasattr(next_initial_state, 'acceleration'):
+                        next_initial_state.acceleration = 0.0
+                    planning_problem_set.planning_problem_dict[pp_id].initial_state = next_initial_state
+                    planning_problem_set.planning_problem_dict[pp_id].goal.state_list[0].time_step.start -= skip_step
+                    planning_problem_set.planning_problem_dict[pp_id].goal.state_list[0].time_step.end   -= skip_step
+
+                else:
+                    raise Exception('Length of trajectory is too short!')
+                # print(f'State time step {next_state.time_step}')
+
+            return trajectory_buffer[step % skip_step]
+
+
+
         def run_simulation():
+
             ego_vehicles = sumo_sim.ego_vehicles
+            pps_ids = list(planning_problem_set.planning_problem_dict.keys())
+            print('Planning problem IDs:', list(planning_problem_set.planning_problem_dict.keys()))
+            print('Ego vehicles\' SUMO IDs:', list(ego_vehicles.keys()))
+            # x = {pps_ids[0]: ego_v for _, ego_v in sumo_sim.ego_vehicles.items()}
+            # print('Reassigned `ego_vehicles` dict:', x)
+            # print(planning_problem_set.planning_problem_dict[pps_ids[0]].goal.state_list[0].time_step)
+            for k, pp in planning_problem_set.planning_problem_dict.items():
+                # pp.goal.state_list[0].time_step.end += 4
+                # pp.goal.state_list[0].time_step.start -= 1
+                pass
+            # print(planning_problem_set.planning_problem_dict[pps_ids[0]].goal.state_list[0].time_step)
+
             for step in range(num_of_steps):
+                # print()
+                # print(f'Processing step {step+1:03d} in {num_of_steps} steps.')
+                # print(f'Goal time step interval: {planning_problem_set.planning_problem_dict[pps_ids[0]].goal.state_list[0].time_step.start} {planning_problem_set.planning_problem_dict[pps_ids[0]].goal.state_list[0].time_step.end}')
+
                 if use_sumo_manager:
                     ego_vehicles = sumo_sim.ego_vehicles
 
                 # retrieve the CommonRoad scenario at the current time step, e.g. as an input for a prediction module
-                current_scenario = sumo_sim.commonroad_scenario_at_time_step(sumo_sim.current_time_step)
-                for idx, ego_vehicle in enumerate(ego_vehicles.values()):
+                current_scenario = sumo_sim.commonroad_scenario_at_time_step(sumo_sim.current_time_step, start_0=True)
+                # print(type(current_scenario))
+                # print(current_scenario)
+                for idx, ego_vehicle in enumerate(ego_vehicles.values()): # Same as looping over planning problem set.
                     # retrieve the current state of the ego vehicle
                     state_current_ego = ego_vehicle.current_state
+                    # print(idx, type(state_current_ego), state_current_ego)
 
                     # ====== plug in your motion planner here
+                    if step == 0:
+                        mod_init_state = copy.deepcopy(planning_problem_set.planning_problem_dict[pps_ids[idx]].initial_state)
+                        mod_init_state.time_step = 1
+                        trajectory_ego = [mod_init_state]
+                        ego_vehicle.set_planned_trajectory(trajectory_ego)
                     # example motion planner which decelerates to full stop
-                    next_state = copy.deepcopy(state_current_ego)
-                    next_state.steering_angle = 0.0
-                    a = -4.0
-                    dt = 0.1
-                    if next_state.velocity > 0:
-                        v = next_state.velocity
-                        x, y = next_state.position
-                        o = next_state.orientation
+                    # next_state = copy.deepcopy(state_current_ego)
+                    # next_state.steering_angle = 0.0
+                    # a = -4.0
+                    # dt = 0.1
+                    # if next_state.velocity > 0:
+                    #     v = next_state.velocity
+                    #     x, y = next_state.position
+                    #     o = next_state.orientation
 
-                        next_state.position = np.array([x + v * cos(o) * dt, y + v * sin(o) * dt])
-                        next_state.velocity += a * dt
+                    #     next_state.position = np.array([x + v * cos(o) * dt, y + v * sin(o) * dt])
+                    #     next_state.velocity += a * dt
+                    next_state = get_next_state(current_scenario, trajectory_buffer, pps_ids[idx], step)
                     # ====== end of motion planner
 
                     # update the ego vehicle with new trajectory with only 1 state for the current step
@@ -122,6 +240,23 @@ def simulate_scenario(mode: SimulationOption,
                     sumo_sim.ego_vehicles = ego_vehicles
 
                 sumo_sim.simulate_step()
+
+            # Restore initial state after simulation.
+            if use_sumo_manager:
+                ego_vehicles = sumo_sim.ego_vehicles
+            for idx, ego_vehicle in enumerate(ego_vehicles.values()):
+                pp_id = pps_ids[idx]
+                if pp_id in pp_orig_init_states:
+                    next_initial_state = pp_orig_init_states.pop(pp_id)
+                    if not hasattr(next_initial_state, 'yaw_rate'):
+                        next_initial_state.yaw_rate = 0.0
+                    if not hasattr(next_initial_state, 'slip_angle'):
+                        next_initial_state.slip_angle = 0.0
+                    if not hasattr(next_initial_state, 'acceleration'):
+                        next_initial_state.acceleration = 0.0
+                    planning_problem_set.planning_problem_dict[pp_id].initial_state = next_initial_state
+                    planning_problem_set.planning_problem_dict[pp_id].goal.state_list[0].time_step.end   = pp_orig_time_ends.pop(pp_id)
+                    planning_problem_set.planning_problem_dict[pp_id].goal.state_list[0].time_step.start = pp_orig_time_starts.pop(pp_id)
 
         run_simulation()
 
@@ -138,6 +273,13 @@ def simulate_scenario(mode: SimulationOption,
                     # update the ego vehicles with solution trajectories
                     trajectory_solution = solution.planning_problem_solutions[idx_ego].trajectory
                     next_state = copy.deepcopy(trajectory_solution.state_list[time_step])
+
+                    if time_step == 0:
+                        pps_ids = list(planning_problem_set.planning_problem_dict.keys())
+                        mod_init_state = copy.deepcopy(planning_problem_set.planning_problem_dict[pps_ids[idx_ego]].initial_state)
+                        mod_init_state.time_step = 1
+                        trajectory_ego = [mod_init_state]
+                        ego_vehicle.set_planned_trajectory(trajectory_ego)
 
                     next_state.time_step = 1
                     trajectory_ego = [next_state]
@@ -158,12 +300,18 @@ def simulate_scenario(mode: SimulationOption,
     # stop the simulation
     sumo_sim.stop()
     ego_vehicles = {list(planning_problem_set.planning_problem_dict.keys())[0]:
-                        ego_v for _, ego_v in sumo_sim.ego_vehicles.items()}
+                         ego_v for _, ego_v in sumo_sim.ego_vehicles.items()}
+    reindexed_ego_vehicles = {}
+    for idx, (pp_id, _) in enumerate(planning_problem_set.planning_problem_dict.items()):
+        reindexed_ego_vehicles[pp_id] = list(sumo_sim.ego_vehicles.values())[idx]
 
     if use_sumo_manager:
         sumo_interface.stop_simulator()
 
-    return simulated_scenario, ego_vehicles
+    print()
+
+    return simulated_scenario, reindexed_ego_vehicles#ego_vehicles
+
 
 
 def simulate_without_ego(interactive_scenario_path: str,
@@ -212,8 +360,7 @@ def simulate_with_solution(interactive_scenario_path: str,
                            solution: Solution = None,
                            create_video: bool = False,
                            use_sumo_manager: bool = False,
-                           create_ego_obstacle: bool = False) \
-        -> Tuple[Scenario, PlanningProblemSet, Dict[int, EgoVehicle]]:
+                           create_ego_obstacle: bool = False) -> Tuple[Scenario, PlanningProblemSet, Dict[int, EgoVehicle]]:
     """
     Simulates an interactive scenario with a given solution
 
